19ff2369ba6b0142174de81d576c740fee9f0057
Onkar-nst <onkardange15@gmail.com>
2025-11-27 23:16:58 +0530

    connected the problems and submission routes to server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/authRoutes");
+const problemRoutes = require("./routes/problemRoutes");
 const battleRoutes = require("./routes/battleRoutes");
+const submissionRoutes = require("./routes/submissionRoutes");
 const run_code = require("./routes/run_code");
 
 dotenv.config();
@@ -12,7 +14,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
+app.use("/problems", problemRoutes);
 app.use("/battles", battleRoutes);
+app.use("/submissions", submissionRoutes);
 app.use("/", run_code);
 
 const PORT = process.env.PORT || 3000;
